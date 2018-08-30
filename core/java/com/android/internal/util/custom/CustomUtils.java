747df43e403975e0a8ac6a0795bda31c423e240e
romanbb <romanbirg@gmail.com>
2018-08-30 11:04:47 +0200

    base: add isPackageInstalled boolean
    Change-Id: Ib23bdb4cf2f8f0ed909f52ac2dab6e18fe9a91d4

--- core/java/com/android/internal/util/custom/CustomUtils.java
+++ core/java/com/android/internal/util/custom/CustomUtils.java
@@ -23,6 +23,9 @@ import android.os.RemoteException;
 import android.os.ServiceManager;
 import android.os.SystemClock;
 
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager.NameNotFoundException;
+
 import com.android.internal.statusbar.IStatusBarService;
 
 /**
@@ -35,6 +38,25 @@ public class CustomUtils {
         if (pm!= null) {
             pm.goToSleep(SystemClock.uptimeMillis());
         }
+    }
+
+    public static boolean isPackageInstalled(Context context, String pkg, boolean ignoreState) {
+        if (pkg != null) {
+            try {
+                PackageInfo pi = context.getPackageManager().getPackageInfo(pkg, 0);
+                if (!pi.applicationInfo.enabled && !ignoreState) {
+                    return false;
+                }
+            } catch (NameNotFoundException e) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static boolean isPackageInstalled(Context context, String pkg) {
+        return isPackageInstalled(context, pkg, true);
     }
 
     public static boolean deviceHasFlashlight(Context ctx) {
