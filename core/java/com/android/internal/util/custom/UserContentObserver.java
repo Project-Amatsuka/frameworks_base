ArrowOS/android_frameworks_base

base: Squashed import of OmniJaws integration
This is a squash of the following:

Author: maxwen <max.weninger@gmail.com>
Date:   Sat Feb 25 17:31:05 2017 +0100
base: SystemUI: OmniJaws qs tile integration

Change-Id: I4a00255a362298820f9f7997b3089e135358e0f6

OmniJaws: Fix More Settings Intent

Change-Id: I982f09d6ba5beadb5c38eb52029eff1b95cec04c

base: qs weather: be nice to people without OmniJaws

if OmniJaws is not available or disabled show an informative
Toast. Also change the default tile image to show also some sun :)

Change-Id: Id1216dfc2aa5a9cf86e1b6accaf354f25c5541fd

base: SystemUI: remove force weather update bits

force updates have been removed from service API

Change-Id: I183bb1a3990d28211d99ff024f9faf59faa6ac4b

base: SystemUI: use new OmniJaws error broadcasts

Change-Id: I3c7da3843186897b7c93d477ca0696a39e0ada65

base: SystemUI: dont hide weather forcast data on errors

only show error in header
see https://gerrit.omnirom.org/23116 for details

Change-Id: Iaa2fa7b6e0d6638b142b19149c5b647d64f3b2e0

base: SystemUI: dont show OmniJaws error that prominent

instead just show a small * to indicate an error. In best case
the update has been triggered on screen on anyway and will
be updated soon

Change-Id: I31446c8339e8093e41f9fc8a2788b251bcd3dced

base: SystemUI: add dedicated TextView for OmniJaws error marker

dont mess with the layout in the error case
and fix proper layout handling for loooong city names

Change-Id: I52919421cbb550e2a662e7df65118d4b03dcebf4
Signed-off-by: Shubham Singh <coolsks94@gmail.com>

Author: maxwen <max.weninger@gmail.com>
Date:   Sun Oct 8 01:42:30 2017 +0200
base: SystemUI: change disabled QS weather tile image

Change-Id: I107ccabf655c7b9e099fac165cc46d88f009d3f7

commit 5e01b70606ff1e8d946b7e5dec18cdda65f32c2c
Author: maxwen <max.weninger@gmail.com>
base: SystemUI: add tinting for OmniJaws provided QS images

Change-Id: I3fb78564af673d4011b983d88a995dee63134352

Author: maxwen <max.weninger@gmail.com>
Date:   Tue Oct

core/java/com/android/internal/util/arrow/UserContentObserver.java
@@ -1,0 +1,92 @@
+/*
+ * Copyright (C) 2014 The CyanogenMod Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License
+ */
+package com.android.internal.utils.arrow;
+
+import android.app.ActivityManagerNative;
+import android.app.IUserSwitchObserver;
+import android.database.ContentObserver;
+import android.net.Uri;
+import android.os.Handler;
+import android.os.IRemoteCallback;
+import android.os.RemoteException;
+import android.util.Log;
+
+/**
+ * Simple extension of ContentObserver that also listens for user switch events to call update
+ */
+public abstract class UserContentObserver extends ContentObserver {
+    private static final String TAG = "UserContentObserver";
+
+    private Runnable mUpdateRunnable;
+
+    private IUserSwitchObserver mUserSwitchObserver = new IUserSwitchObserver.Stub() {
+        @Override
+        public void onUserSwitching(int newUserId, IRemoteCallback reply) {
+        }
+        @Override
+        public void onUserSwitchComplete(int newUserId) throws RemoteException {
+            mHandler.post(mUpdateRunnable);
+        }
+        @Override
+        public void onForegroundProfileSwitch(int newProfileId) {
+        }
+        @Override
+        public void onLockedBootComplete(int val) {
+        }
+    };
+
+    private Handler mHandler;
+
+    public UserContentObserver(Handler handler) {
+        super(handler);
+        mHandler = handler;
+        mUpdateRunnable = new Runnable() {
+            @Override
+            public void run() {
+                update();
+            }
+        };
+    }
+
+    protected void observe() {
+        try {
+            ActivityManagerNative.getDefault().registerUserSwitchObserver(mUserSwitchObserver, TAG);
+        } catch (RemoteException e) {
+            Log.w(TAG, "Unable to register user switch observer!", e);
+        }
+    }
+
+    protected void unobserve() {
+        try {
+            mHandler.removeCallbacks(mUpdateRunnable);
+            ActivityManagerNative.getDefault().unregisterUserSwitchObserver(mUserSwitchObserver);
+        } catch (RemoteException e) {
+            Log.w(TAG, "Unable to unregister user switch observer!", e);
+        }
+    }
+
+    protected abstract void update();
+
+    @Override
+    public void onChange(boolean selfChange) {
+        update();
+    }
+
+    @Override
+    public void onChange(boolean selfChange, Uri uri) {
+        update();
+    }
+}

Project-Amatsuka/frameworks_base

core/java/com/android/internal/util/custom/UserContentObserver.java
@@ -1,0 +1,92 @@
+/*
+ * Copyright (C) 2014 The CyanogenMod Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License
+ */
+package com.android.internal.utils.custom;
+
+import android.app.ActivityManagerNative;
+import android.app.IUserSwitchObserver;
+import android.database.ContentObserver;
+import android.net.Uri;
+import android.os.Handler;
+import android.os.IRemoteCallback;
+import android.os.RemoteException;
+import android.util.Log;
+
+/**
+ * Simple extension of ContentObserver that also listens for user switch events to call update
+ */
+public abstract class UserContentObserver extends ContentObserver {
+    private static final String TAG = "UserContentObserver";
+
+    private Runnable mUpdateRunnable;
+
+    private IUserSwitchObserver mUserSwitchObserver = new IUserSwitchObserver.Stub() {
+        @Override
+        public void onUserSwitching(int newUserId, IRemoteCallback reply) {
+        }
+        @Override
+        public void onUserSwitchComplete(int newUserId) throws RemoteException {
+            mHandler.post(mUpdateRunnable);
+        }
+        @Override
+        public void onForegroundProfileSwitch(int newProfileId) {
+        }
+        @Override
+        public void onLockedBootComplete(int val) {
+        }
+    };
+
+    private Handler mHandler;
+
+    public UserContentObserver(Handler handler) {
+        super(handler);
+        mHandler = handler;
+        mUpdateRunnable = new Runnable() {
+            @Override
+            public void run() {
+                update();
+            }
+        };
+    }
+
+    protected void observe() {
+        try {
+            ActivityManagerNative.getDefault().registerUserSwitchObserver(mUserSwitchObserver, TAG);
+        } catch (RemoteException e) {
+            Log.w(TAG, "Unable to register user switch observer!", e);
+        }
+    }
+
+    protected void unobserve() {
+        try {
+            mHandler.removeCallbacks(mUpdateRunnable);
+            ActivityManagerNative.getDefault().unregisterUserSwitchObserver(mUserSwitchObserver);
+        } catch (RemoteException e) {
+            Log.w(TAG, "Unable to unregister user switch observer!", e);
+        }
+    }
+
+    protected abstract void update();
+
+    @Override
+    public void onChange(boolean selfChange) {
+        update();
+    }
+
+    @Override
+    public void onChange(boolean selfChange, Uri uri) {
+        update();
+    }
+}