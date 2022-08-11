df504e6dd61965f7c80506398459f5f0d150424a
Col_or <col_or@qq.com>
2022-08-11 15:04:18 +0800

    SystemUI: Add a drag handle line in QuickQS
    Signed-off-by: Col_or <col_or@qq.com>
    Change-Id: I0caa69eb5afb7be1529a5d2210efe9106d0c8622

--- packages/SystemUI/src/com/android/systemui/qs/QuickStatusBarHeader.java
+++ packages/SystemUI/src/com/android/systemui/qs/QuickStatusBarHeader.java
@@ -85,6 +85,7 @@ public class QuickStatusBarHeader extends FrameLayout implements TunerService.Tu
     private View mRightLayout;
     private View mDateContainer;
     private View mPrivacyContainer;
+    private View mQSDragHandle;
 
     private BatteryMeterView mBatteryRemainingIcon;
     private StatusIconContainer mIconContainer;
@@ -149,6 +150,8 @@ public class QuickStatusBarHeader extends FrameLayout implements TunerService.Tu
         mDatePrivacySeparator = findViewById(R.id.space);
         // Tint for the battery icons are handled in setupHost()
         mBatteryRemainingIcon = findViewById(R.id.batteryRemainingIcon);
+
+        mQSDragHandle = findViewById(R.id.qs_drag_handle);
 
         updateResources();
         Configuration config = mContext.getResources().getConfiguration();
@@ -340,6 +343,7 @@ public class QuickStatusBarHeader extends FrameLayout implements TunerService.Tu
                 .addFloat(mDateView, "alpha", 0, 0, 1)
                 .addFloat(mClockDateView, "alpha", 1, 0, 0)
                 .addFloat(mQSCarriers, "alpha", 0, 1)
+                .addFloat(mQSDragHandle, "alpha", 1, 0)
                 .setListener(new TouchAnimator.ListenerAdapter() {
                     @Override
                     public void onAnimationAtEnd() {
