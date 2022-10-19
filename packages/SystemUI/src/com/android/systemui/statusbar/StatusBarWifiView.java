4240c2f7f45b068b8425a5bab2ee204a22688f1e
ReallySnow <reallysnow233@gmail.com>
2022-10-20 00:43:48 +0800

    SystemUI: Make Wifi/Hotspot standard controllable [1/2]
    * Use settings to control Wifi/Hotspot display in status bar
    * Inspired by https://github.com/AOSPA/android_frameworks_base/commit/336756ec426ce89b69805c925885e0f0baad4b58
    
    Change-Id: I1f8c5f55f76fe2a2b57fc62d7469808fae09ce66

--- packages/SystemUI/src/com/android/systemui/statusbar/StatusBarWifiView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/StatusBarWifiView.java
@@ -24,6 +24,7 @@ import static com.android.systemui.statusbar.StatusBarIconView.STATE_ICON;
 import android.content.Context;
 import android.content.res.ColorStateList;
 import android.graphics.Rect;
+import android.provider.Settings;
 import android.util.AttributeSet;
 import android.view.Gravity;
 import android.view.LayoutInflater;
@@ -239,11 +240,17 @@ public class StatusBarWifiView extends FrameLayout implements DarkReceiver,
     }
 
     private void setWifiStandard() {
+        if (Settings.Secure.getInt(Context.getContentResolver(),
+                Settings.Secure.SHOW_WIFI_STANDARD_ICON, 1) == 0) {
+            mWifiStandard.setVisibility(View.GONE);
+            return;
+        }
         int wifiStandard = mState.wifiStandard;
         if (wifiStandard >= 4) {
             int identifier = getResources().getIdentifier("ic_wifi_standard_" + wifiStandard,
                     "drawable", getContext().getPackageName());
             if (identifier > 0) {
+                mWifiStandard.setVisibility(View.VISIBLE);
                 mWifiStandard.setImageDrawable(mContext.getDrawable(identifier));
             }
         }
