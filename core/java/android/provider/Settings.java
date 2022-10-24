3c87ca0834f98435ba7c9b43884c38293766482e
ReallySnow <reallysnow233@gmail.com>
2022-10-24 19:24:27 +0800

    fixup! Wallpaper: Make ImageWallpaper zooming tunable
    * Tuner should use Secure instead of System by default
    
    Change-Id: Ie28d1e9b3d6a5e31d60bc6ace7bd514bd38e8e0a

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5526,13 +5526,6 @@ public final class Settings {
          */
 
         /**
-         *  Toggle the wallpaper zoom effects
-         *  0 = 0ff, 1 = on
-         * @hide
-         */
-        public static final String DISPLAY_SETTINGS_WALLPAPER_ZOOM = "display_settings_wallpaper_zoom";
-
-        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
@@ -10870,6 +10863,13 @@ public final class Settings {
          * @hide
          */
         public static final String SHOW_WIFI_STANDARD_ICON = "show_wifi_standard_icon";
+
+        /**
+         *  Toggle the wallpaper zoom effects
+         *  0 = 0ff, 1 = on
+         * @hide
+         */
+        public static final String DISPLAY_SETTINGS_WALLPAPER_ZOOM = "display_settings_wallpaper_zoom";
 
         /**
          * 
