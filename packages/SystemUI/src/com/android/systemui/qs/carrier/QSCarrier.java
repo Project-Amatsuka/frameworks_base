Project-Amatsuka/frameworks_base

commit 05d8fcc466419d8b4b0ca273449213bae858f9e4

SystemUI: Follow light/dark theme in SplitShade Header
* Google's default implementation is dark, which means
  it doesn't need to follow the light/dark color change,
  but we broke it, so add it to complete the SplitShade
  Header color change

str: Forward-ported to T, code changes (initialization etc)
Co-authored-by: Col_or <col_or@qq.com>
Change-Id: I5464039885197eeb43bd31b822bfcba7a1b08776
Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
Signed-off-by: str-br <alexfinhart@gmail.com>

packages/SystemUI/src/com/android/systemui/qs/carrier/QSCarrier.java
@@ -140,6 +140,14 @@ public class QSCarrier extends LinearLayout {
                         com.android.settingslib.R.string.not_default_data_content_description));
     }
 
+    public void updateColors(ColorStateList colorStateList) {
+        final boolean visible = !mIsSingleCarrier;
+        if (visible) {
+            mMobileRoaming.setImageTintList(colorStateList);
+            mMobileSignal.setImageTintList(colorStateList);
+        }
+    }
+
     @VisibleForTesting
     View getRSSIView() {
         return mMobileGroup;

packages/SystemUI/src/com/android/systemui/qs/carrier/QSCarrierGroup.java
@@ -18,8 +18,11 @@ package com.android.systemui.qs.carrier;
 
 import android.annotation.StyleRes;
 import android.content.Context;
+import android.content.res.ColorStateList;
+import android.graphics.PorterDuff;
 import android.util.AttributeSet;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -56,6 +59,19 @@ public class QSCarrierGroup extends LinearLayout {
 
     View getCarrierDivider2() {
         return findViewById(R.id.qs_carrier_divider2);
+    }
+
+    public void updateColors(int color, ColorStateList colorStateList) {
+        getNoSimTextView().setTextColor(color);
+        QSCarrier[] qsCarriers = { getCarrier1View(), getCarrier2View(), getCarrier3View() };
+        for (QSCarrier qsCarrier : qsCarriers) {
+            for (int i = 0; i < qsCarrier.getChildCount(); i++) {
+                qsCarrier.updateColors(colorStateList);
+                if (qsCarrier.getChildAt(i) instanceof TextView) {
+                    ((TextView) qsCarrier.getChildAt(i)).setTextColor(color);
+                }
+            }
+        }
     }
 
     public void updateTextAppearance(@StyleRes int resId) {

packages/SystemUI/src/com/android/systemui/statusbar/phone/LargeScreenShadeHeaderController.kt
@@ -16,6 +16,9 @@
 
 package com.android.systemui.statusbar.phone
 
+import android.content.Context
+import android.content.res.ColorStateList
+import android.graphics.Color
 import android.app.StatusBarManager
 import android.view.View
 import android.widget.TextView
@@ -37,6 +40,7 @@ import com.android.systemui.qs.carrier.QSCarrierGroupController
 import com.android.systemui.statusbar.phone.dagger.CentralSurfacesComponent.CentralSurfacesScope
 import com.android.systemui.statusbar.phone.dagger.StatusBarViewModule.LARGE_SCREEN_BATTERY_CONTROLLER
 import com.android.systemui.statusbar.phone.dagger.StatusBarViewModule.LARGE_SCREEN_SHADE_HEADER
+import com.android.systemui.statusbar.policy.Clock
 import com.android.systemui.statusbar.policy.ConfigurationController
 import java.io.PrintWriter
 import javax.inject.Inject
@@ -48,6 +52,7 @@ class LargeScreenShadeHeaderController @Inject constructor(
     private val statusBarIconController: StatusBarIconController,
     private val privacyIconsController: HeaderPrivacyIconsController,
     private val configurationController: ConfigurationController,
+    private val context: Context,
     qsCarrierGroupControllerBuilder: QSCarrierGroupController.Builder,
     featureFlags: FeatureFlags,
     @Named(LARGE_SCREEN_BATTERY_CONTROLLER) batteryMeterViewController: BatteryMeterViewController,
@@ -72,10 +77,12 @@ class LargeScreenShadeHeaderController @Inject constructor(
     private val combinedHeaders = featureFlags.isEnabled(Flags.COMBINED_QS_HEADERS)
     private val iconManager: StatusBarIconController.TintedIconManager
     private val iconContainer: StatusIconContainer
+    private var textColorPrimary = Color.TRANSPARENT
     private val carrierIconSlots: List<String>
     private val qsCarrierGroupController: QSCarrierGroupController
     private val clock: TextView = header.findViewById(R.id.clock)
     private val date: TextView = header.findViewById(R.id.date)
+    private val batteryIcon: BatteryMeterView = header.findViewById(R.id.batteryRemainingIcon)
     private val qsCarrierGroup: QSCarrierGroup = header.findViewById(R.id.carrier_group)
 
     private var qsDisabled = false
@@ -160,7 +167,6 @@ class LargeScreenShadeHeaderController @Inject constructor(
         bindConfigurationListener()
 
         batteryMeterViewController.init()
-        val batteryIcon: BatteryMeterView = header.findViewById(R.id.batteryRemainingIcon)
 
         // battery settings same as in QS icons
         batteryMeterViewController.ignoreTunerUpdates()
@@ -183,10 +189,37 @@ class LargeScreenShadeHeaderController @Inject constructor(
                 .setQSCarrierGroup(header.findViewById(R.id.carrier_group))
                 .build()
 
+        val configurationChangedListener = object : ConfigurationController.ConfigurationListener {
+            override fun onUiModeChanged() {
+                updateResources()
+            }
+        }
+        configurationController.addCallback(configurationChangedListener)
+
         dumpManager.registerDumpable(this)
 
         updateVisibility()
         updateConstraints()
+        updateResources()
+    }
+
+    private fun updateResources() {
+        val fillColor = Utils.getColorAttrDefaultColor(context, android.R.attr.textColorPrimary)
+        iconManager.setTint(fillColor)
+        val textColor = Utils.getColorAttrDefaultColor(context, android.R.attr.textColorPrimary)
+        val colorStateList = Utils.getColorAttr(context, android.R.attr.textColorPrimary)
+        if (textColor != textColorPrimary) {
+            val textColorSecondary = Utils.getColorAttrDefaultColor(context,
+                    android.R.attr.textColorSecondary)
+            textColorPrimary = textColor
+            if (iconManager != null) {
+                iconManager.setTint(textColor)
+            }
+            clock.setTextColor(textColorPrimary)
+            date.setTextColor(textColorPrimary)
+            qsCarrierGroup.updateColors(textColorPrimary, colorStateList)
+            batteryIcon.updateColors(textColorPrimary, textColorSecondary, textColorPrimary)
+        }
     }
 
     fun disable(state1: Int, state2: Int, animate: Boolean) {
@@ -222,6 +255,7 @@ class LargeScreenShadeHeaderController @Inject constructor(
         }
         updateVisibility()
         updatePosition()
+        updateResources()
     }
 
     private fun onHeaderStateChanged() {
@@ -232,6 +266,7 @@ class LargeScreenShadeHeaderController @Inject constructor(
         }
         updateVisibility()
         updateConstraints()
+        updateResources()
     }
 
     private fun updateVisibility() {

commit 2c23bb259fdd73c450fe795f779b55054c71b3b5

SystemUI: Remove visibility check in setting QSCarrier color
This fixes a corner case where the signal icon color is incorrect:
Have dual sim -> switch to dark theme -> turn off one sim -> switch
to light theme -> turn the other sim back on. QS carrier signal
is colored white instead of black.

Ensure that the signal icon color is always up to date, by removing
visibility check, to avoid this issue.

Fixes: 05d8fcc ("Follow light/dark theme in SplitShade Header")
Change-Id: I092c06053fc4bc8d9ca51d1d31128da27ef6a823

packages/SystemUI/src/com/android/systemui/qs/carrier/QSCarrier.java
@@ -131,11 +131,8 @@ public class QSCarrier extends LinearLayout {
     }
 
     public void updateColors(ColorStateList colorStateList) {
-        final boolean visible = !mIsSingleCarrier;
-        if (visible) {
-            mMobileRoaming.setImageTintList(colorStateList);
-            mMobileSignal.setImageTintList(colorStateList);
-        }
+        mMobileRoaming.setImageTintList(colorStateList);
+        mMobileSignal.setImageTintList(colorStateList);
     }
 
     @VisibleForTesting